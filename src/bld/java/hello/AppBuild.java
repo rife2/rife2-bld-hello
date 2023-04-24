553bc2cf0e5351e64a51e9631b81977070d70acc
Geert Bevin <gbevin@uwyn.com>
2023-04-23 20:36:16 -0400

    Updated to RIFE2 and bld 1.6.1

--- src/bld/java/hello/AppBuild.java
+++ src/bld/java/hello/AppBuild.java
@@ -22,7 +22,7 @@ public class AppBuild extends WebProject {
 
         repositories = List.of(MAVEN_CENTRAL, RIFE2_RELEASES);
         scope(compile)
-            .include(dependency("com.uwyn.rife2", "rife2", version(1,6,0)));
+            .include(dependency("com.uwyn.rife2", "rife2", version(1,6,1)));
         scope(test)
             .include(dependency("org.jsoup", "jsoup", version(1,15,4)))
             .include(dependency("org.junit.jupiter", "junit-jupiter", version(5,9,2)))
