2b470630feff32812761435dca75f776fa70dd48
Geert Bevin <gbevin@uwyn.com>
2024-05-01 21:24:48 -0400

    Updated to bld 1.9.1. Updated dependencies.

--- src/bld/java/hello/AppBuild.java
+++ src/bld/java/hello/AppBuild.java
@@ -28,9 +28,9 @@ public class AppBuild extends WebProject {
             .include(dependency("org.junit.jupiter", "junit-jupiter", version(5,10,2)))
             .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1,10,2)));
         scope(standalone)
-            .include(dependency("org.eclipse.jetty.ee10", "jetty-ee10", version(12,0,6)))
-            .include(dependency("org.eclipse.jetty.ee10", "jetty-ee10-servlet", version(12,0,6)))
-            .include(dependency("org.slf4j", "slf4j-simple", version(2,0,12)));
+            .include(dependency("org.eclipse.jetty.ee10", "jetty-ee10", version(12,0,8)))
+            .include(dependency("org.eclipse.jetty.ee10", "jetty-ee10-servlet", version(12,0,8)))
+            .include(dependency("org.slf4j", "slf4j-simple", version(2,0,13)));
 
         precompileOperation().templateTypes(HTML);
     }
